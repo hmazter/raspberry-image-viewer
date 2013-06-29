04304bb86a82cff4b009c9b6cf0b93e6f588727f
Kristoffer Hogberg <krihog@gmail.com>
2013-06-29 14:03:10 +0200

    Copy all jpg images, show slideshow

--- showimages.sh
+++ showimages.sh
@@ -4,11 +4,11 @@ echo "mounting usb"
 sudo /home/pi/mountusb.sh
 
 # copy images
-echo "copy hillsong1.jpg"
-cp /home/pi/usbmount/hillsong1.jpg /home/pi/images/
+echo "copy *.jpg from USB"
+cp /home/pi/usbmount/*.jpg /home/pi/images/
 
 echo "unmount usb"
 sudo /home/pi/unmountusb.sh
 
-echo "showing images/hillsong1.jpg"
-/usr/bin/fbi -a -noverbose /home/pi/images/hillsong1.jpg
+echo "showing 5 second slideshow with images/*.jpg"
+/usr/bin/fbi -a -noverbose -t 5 /home/pi/images/*.jpg
